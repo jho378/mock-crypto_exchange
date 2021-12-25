d6035d84cca54d52981b38440c57d967827f068b
jho378 <81404922+jho378@users.noreply.github.com>
2021-12-25 14:24:45 +0900

    Update index.js

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Coin = require('./Coin');
 const Asset = require('./Asset');
 const Key = require('./Key')
 
-const mongoURL = 'mongodb://test0:test0@testmongo-shard-00-00.4vjoj.mongodb.net:27017,testmongo-shard-00-01.4vjoj.mongodb.net:27017,testmongo-shard-00-02.4vjoj.mongodb.net:27017/coinServer?ssl=true&replicaSet=atlas-me699l-shard-0&authSource=admin&retryWrites=true&w=majority'; // coinServer로 이름 바꿈;
+const mongoURL = 'mongodb://test0:<pw>@testmongo-shard-00-00.4vjoj.mongodb.net:27017,testmongo-shard-00-01.4vjoj.mongodb.net:27017,testmongo-shard-00-02.4vjoj.mongodb.net:27017/coinServer?ssl=true&replicaSet=atlas-me699l-shard-0&authSource=admin&retryWrites=true&w=majority'; // coinServer로 이름 바꿈;
 
 mongoose.connect(mongoURL);
 
